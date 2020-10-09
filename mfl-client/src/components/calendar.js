12630f8f4e32c7faba5722d7e4d298fb02d6ff32
JeongWon Liee <khjvvv7@gmail.com>
2020-10-09 15:45:29 +0900

    chore: display first day of week

--- mfl-client/src/components/calendar.js
+++ mfl-client/src/components/calendar.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+const dayOfWeekEn = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const dayOfWeekKo = ["일", "월", "화", "수", "목", "금", "토"];
+
 function CalendarHead(props) {
   return (
-    <div className="CalendarHead" style={{
-      border: '1px solid black'
-    }}>
+    <div
+      className="CalendarHead"
+      style={{
+        border: "1px solid black",
+      }}
+    >
       <p>Calendar Head</p>
       <button onClick={props.onNextMonth}>+</button>
       <button onClick={props.onPrevMonth}>-</button>
-      <h2>{props.date.currentYear}.{props.date.currentMonth}</h2>
+      <h2>
+        {props.date.currentYear}.{props.date.currentMonth}
+      </h2>
     </div>
   );
 }
@@ -16,8 +24,10 @@ function CalendarHead(props) {
 function CalendarBody(props) {
   return (
     <div className="CalendarBody">
-      {console.log("Calendar Body : ", props.firstDayOfWeek)}
       <p>Calendar Body</p>
+      <p>
+        {dayOfWeekEn[props.firstDayOfWeek]}({dayOfWeekKo[props.firstDayOfWeek]})
+      </p>
     </div>
   );
 }
--- mfl-client/src/modules/date-reducer.js
+++ mfl-client/src/modules/date-reducer.js
@@ -10,12 +10,14 @@ const initialState = {
   currentYear: new Date().getFullYear(),
   currentMonth: new Date().getMonth() + 1,
   currentDay: new Date().getDay(),
-  firstDayOfWeek: getFirstDayOfWeek(new Date().getFullYear(), new Date().getMonth())
+  firstDayOfWeek: getFirstDayOfWeek(
+    new Date().getFullYear(),
+    new Date().getMonth()
+  ),
 };
 
 function getFirstDayOfWeek(year, month) {
-  return new Date(year.toString() + "-" + month.toString() + "-1").getDay()
-  
+  return new Date(year.toString() + "-" + month.toString() + "-1").getDay();
 }
 
 function dateReducer(state = initialState, action) {
@@ -24,11 +26,11 @@ function dateReducer(state = initialState, action) {
 
   switch (action.type) {
     case NEXT_MONTH:
-      resultMonth = state.currentMonth + 1
-      break
+      resultMonth = state.currentMonth + 1;
+      break;
     case PREV_MONTH:
-      resultMonth = state.currentMonth - 1
-      break
+      resultMonth = state.currentMonth - 1;
+      break;
   }
 
   if (resultMonth > 12) {
@@ -43,8 +45,8 @@ function dateReducer(state = initialState, action) {
     ...state,
     currentMonth: resultMonth,
     currentYear: resultYear,
-    firstDayOfWeek: getFirstDayOfWeek(resultYear, resultMonth)
-  }
+    firstDayOfWeek: getFirstDayOfWeek(resultYear, resultMonth),
+  };
 }
 
 export default dateReducer;
