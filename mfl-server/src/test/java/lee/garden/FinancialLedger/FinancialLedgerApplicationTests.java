e147e8a4ea2cab96afa9b8bd5acf46b8220e106d
JeongWon Liee <khjvvv7@gmail.com>
2020-10-14 14:14:39 +0900

    feat: make server application

---
+++ mfl-server/src/test/java/lee/garden/FinancialLedger/FinancialLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.garden.FinancialLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
