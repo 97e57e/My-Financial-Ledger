d701cc3f48f18d17bbacd521e2e4f426425fd124
JeongWon Liee <khjvvv7@gmail.com>
2020-10-15 15:17:06 +0900

    config: mariaDB config setting

--- mfl-server/src/main/java/lee/garden/FinancialLedger/FinancialLedgerApplication.java
+++ mfl-server/src/main/java/lee/garden/FinancialLedger/FinancialLedgerApplication.java
@@ -1,13 +1,20 @@
 package lee.garden.FinancialLedger;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class FinancialLedgerApplication {
 
+	private static final String PROPERTIES =
+			"spring.config.location="
+					+"classpath:/application.yml"
+					+",classpath:/application-db.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(FinancialLedgerApplication.class, args);
+		new SpringApplicationBuilder(FinancialLedgerApplication.class)
+				.properties(PROPERTIES)
+				.run(args);
 	}
 
 }
