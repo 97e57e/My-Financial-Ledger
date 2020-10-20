2e6407a7161ab5829305573ef574681e41fdce3b
JeongWon Liee <khjvvv7@gmail.com>
2020-10-20 15:45:52 +0900

    test: login success test

--- mfl-server/src/test/java/lee/garden/FinancialLedger/service/account/AccountServiceTest.java
+++ mfl-server/src/test/java/lee/garden/FinancialLedger/service/account/AccountServiceTest.java
@@ -1,11 +1,19 @@
 package lee.garden.FinancialLedger.service.account;
 
+import lee.garden.FinancialLedger.domain.account.CustomUser;
+import lee.garden.FinancialLedger.dto.account.LogInDTO;
 import lee.garden.FinancialLedger.dto.account.SignUpDTO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +25,48 @@ public class AccountServiceTest {
 
     @Test
     public void 회원가입_성공() {
-        SignUpDTO signUpDTO = new SignUpDTO("khjvvv7@gmail.com", "1234", "가든");
+        //given
+        SignUpDTO signUpDTO = new SignUpDTO("khjvvv7@gmail.com", "rkemsWKd123", "가든");
+
+        //when
         Long userId = accountService.signUp(signUpDTO);
 
+        //then
         assertEquals(Long.valueOf(1), userId);
     }
+
+    @Transactional
+    @Test
+    public void 로그인_성공 () throws Exception {
+        //given
+        SignUpDTO signUpDTO = new SignUpDTO("khjvvv7@gmail.com", "rkemsWKd123", "가든");
+        Long userId = accountService.signUp(signUpDTO);
+
+        //when
+        LogInDTO logInDTO = new LogInDTO("khjvvv7@gmail.com", "rkemsWKd123");
+        CustomUser findUser = accountService.signIn(logInDTO);
+
+        //then
+        assertEquals(userId, findUser.getId());
+    }
+
+    /**
+     * Error Response 정의시까지 보류
+     * */
+    @Transactional
+    @Test
+    public void 로그인_실패 () throws Exception {
+//        //given
+//        SignUpDTO signUpDTO = new SignUpDTO("khjvvv7@gmail.com", "rkemsWKd123", "가든");
+//        Long userId = accountService.signUp(signUpDTO);
+//
+//        //when
+//        LogInDTO logInDTO = new LogInDTO("khjvvv7@gmail.com", "1234");
+//        CustomUser findUser = accountService.signIn(logInDTO);
+//
+//        //then
+//        assertEquals(userId, findUser.getId());
+    }
+
 
 }
